14ffe7e03379497ec28c5dbceaee1efdd357fe5c
sarahbarlis09 <sarahbarlis09@gmail.com>
2022-06-10 03:50:20 +0800

    Add stop confetti

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 15;
 let highscore = 0;
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   //   When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'Input a number!';
+    displayMessage('Input a number!');
 
     //When the input is correct
   } else if (guess === secretNumber) {
     startConfetti();
-    document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number!');
     document.querySelector('.check').disabled = true;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
-    startConfetti();
 
     if (score > highscore) {
       highscore = score;
@@ -31,12 +33,11 @@ document.querySelector('.check').addEventListener('click', function () {
   // When guess is wrong
   else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? 'Too high!' : 'Too low!';
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
       document.querySelector('.check').disabled = true;
       document.querySelector('.score').textContent = 0;
       document.querySelector('body').style.backgroundColor = '#751212';
@@ -45,11 +46,12 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = '15';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.check').disabled = false;
   document.querySelector('.guess').value = '';
+  stopConfetti();
 });
